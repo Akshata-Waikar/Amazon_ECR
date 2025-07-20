44850c9b2317d56d6ab1db9adb4722adbd079d58
Akshata-Waikar <akshataa7733@gmail.com>
2025-07-20 16:38:47 +0530

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Akshata!"
+    return "Hello from docker!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
